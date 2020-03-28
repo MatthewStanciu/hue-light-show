87f9b94f92b06c5cf1c9dc4583abd59222eb17d6
Matthew Stanciu <mattbstanciu@gmail.com>
2020-03-27 23:35:10 -0400

    ???

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new huejay.Client({
   username: process.env.USERNAME
 })
 
-function updateLight(lightId) {
+/*function updateLight(lightId) {
 	client.lights.getById(lightId).then(light => {
 		light.brightness = Math.floor((Math.random() * 204) + 50) // random number between 50 and 254
 		light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
@@ -15,7 +15,26 @@ function updateLight(lightId) {
 		
 		return client.lights.save(light)
 	})
+}*/
+
+function updateLight(lightId) {
+	client.lights.getById(lightId).then(light => {
+		light.brightness = Math.floor((Math.random() * 204) + 50)
+		light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
+		light.saturation = Math.floor((Math.random() * 204) + 50)
+		light.transitionTime = Math.floor((Math.random() * 2) + 3)
+
+		return client.lights.save(light)
+	})
 }
+
+/*setInterval(() => {
+	client.lights.getAll().then(lights => {
+		lights.forEach(light => {
+			if (light >= 3) updateLight(light)
+		})
+	})
+}, 3000)*/
 
 setInterval(() => {
 	client.lights.getAll().then(lights => {
