7dad2185c9fc2b86e0817f173a791b32a9ffde9f
Matthew Stanciu <mattbstanciu@gmail.com>
2020-03-27 23:26:39 -0400

    Fix other random value

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function updateLight(lightId) {
 		light.brightness = Math.floor((Math.random() * 204) + 50) // random number between 50 and 254
 		light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
 		light.saturation = Math.floor((Math.random() * 204) + 50)
-		light.transitionTime = Math.floor((Math.random() * 5) + 3)
+		light.transitionTime = Math.floor((Math.random() * 2) + 3) // between 3 and 5
 		
 		return client.lights.save(light)
 	})
