cacfa77acff7cb4b3b411450d66e5b9c186ff586
Matthew Stanciu <mattbstanciu@gmail.com>
2020-03-27 23:23:19 -0400

    Dramatically clean up code

--- index.js
+++ index.js
@@ -7,60 +7,8 @@ const client = new huejay.Client({
   username: process.env.USERNAME
 })
 
-client.lights.getAll().then(lights => {
-	lights.forEach(light => console.log(`Light [${light.id}]: ${light.name}`))
-})
-
-/*client.lights.getById(4).then(light => {
-	light.brightness = 254
-  light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
-	light.saturation = 254
-	light.transitionTime = 3
-	
-	return client.lights.save(light)
-})
-.then(light => {
-	console.log(`Updated light [${light.id}]`);
-})
-.catch(error => {
-	console.log('Something went wrong');
-	console.log(error.stack);
-});*/
-
-function updateLights() {
-	client.lights.getById(3).then(light => {
-		light.brightness = Math.floor((Math.random() * 254) + 50)
-		light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
-		light.saturation = Math.floor((Math.random() * 254) + 50)
-		light.transitionTime = Math.floor((Math.random() * 5) + 3)
-		
-		return client.lights.save(light)
-	})
-	client.lights.getById(4).then(light => {
-		light.brightness = Math.floor((Math.random() * 254) + 50)
-		light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
-		light.saturation = Math.floor((Math.random() * 254) + 50)
-		light.transitionTime = Math.floor((Math.random() * 5) + 3)
-		
-		return client.lights.save(light)
-	})
-	client.lights.getById(5).then(light => {
-		light.brightness = Math.floor((Math.random() * 254) + 50)
-		light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
-		light.saturation = Math.floor((Math.random() * 254) + 50)
-		light.transitionTime = Math.floor((Math.random() * 5) + 3)
-		
-		return client.lights.save(light)
-	})
-	client.lights.getById(6).then(light => {
-		light.brightness = Math.floor((Math.random() * 254) + 50)
-		light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
-		light.saturation = Math.floor((Math.random() * 254) + 50)
-		light.transitionTime = Math.floor((Math.random() * 5) + 3)
-		
-		return client.lights.save(light)
-	})
-	client.lights.getById(7).then(light => {
+function updateLight(lightId) {
+	client.lights.getById(lightId).then(light => {
 		light.brightness = Math.floor((Math.random() * 254) + 50)
 		light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
 		light.saturation = Math.floor((Math.random() * 254) + 50)
@@ -70,6 +18,12 @@ function updateLights() {
 	})
 }
 
-setInterval(() => updateLights(), 3000)
+setInterval(() => {
+	client.lights.getAll().then(lights => {
+		lights.forEach(light => {
+			if (light >= 3) updateLight(light)
+		})
+	})
+}, 3000)
 
 http.createServer().listen(3000)
