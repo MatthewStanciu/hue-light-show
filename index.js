b7a179f81de9b40695d70298951cced5e89fecb4
Matthew Stanciu <mattbstanciu@gmail.com>
2020-03-27 23:36:19 -0400

    my indentation looks weird on github and idk why!!!!!!

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ const client = new huejay.Client({
   username: process.env.USERNAME
 })
 
-/*function updateLight(lightId) {
-	client.lights.getById(lightId).then(light => {
-		light.brightness = Math.floor((Math.random() * 204) + 50) // random number between 50 and 254
-		light.xy = [(Math.random() * 0.7), (Math.random() * 0.8)]
-		light.saturation = Math.floor((Math.random() * 204) + 50)
-		light.transitionTime = Math.floor((Math.random() * 2) + 3) // between 3 and 5
-		
-		return client.lights.save(light)
-	})
-}*/
-
 function updateLight(lightId) {
 	client.lights.getById(lightId).then(light => {
 		light.brightness = Math.floor((Math.random() * 204) + 50)
@@ -27,14 +16,6 @@ function updateLight(lightId) {
 		return client.lights.save(light)
 	})
 }
-
-/*setInterval(() => {
-	client.lights.getAll().then(lights => {
-		lights.forEach(light => {
-			if (light >= 3) updateLight(light)
-		})
-	})
-}, 3000)*/
 
 setInterval(() => {
 	client.lights.getAll().then(lights => {
